f6aa1ea92e99d009aebfc4c3b04228f433f1ef3d
basboyukhabip <habibasb@student.arteveldehs.be>
2025-09-07 21:37:16 +0200

    added content of homepage with correct components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import './App.css'
 
+import './styles/App.css'
+import Training from './components/training.jsx'
+import Footer from './components/footer.jsx'
 function App() {
 
   return (
     <>
-      
+      <h1>GymTrack <br /> Maatje</h1>
+      <p>Track your workouts and prestations</p>
+      <button>New workout</button>
+
+      <Training />
+      <Training />
+      <Training />
+      <Footer />
     </>
   )
 }
---
+++ src/components/training.jsx
@@ -0,0 +1,19 @@
+function Training () {
+
+    return ( 
+        <>
+        <section>
+            <div>
+                <img src="" alt="" />
+                <button> <img src="" alt="" /></button>
+            </div>
+
+            <p>Chest & bicep</p>
+            <p>4 excercises</p>
+        </section>
+        </>
+
+     );
+}
+
+export default Training;
